eedfb27db02be77536239c47c9d340960b681144
Poliana <polianariveiro@gmail.com>
2024-02-10 03:54:15 -0300

    add elementos no form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import Form from "./components/Form"
 import Navbar from "./components/Navbar"
 import Table from "./components/Table"
 
 function App() {
 
+  const data = localStorage.getItem("jogadores");
+  const [jogadorList, setJogadorList] = useState( data ? JSON.parse(data) : []);
+
 
   return (
     <>
     <Navbar />
-     <Form />
-     <Table />
+     <Form jogadorList={jogadorList} setJogadorList={setJogadorList} />
+     <Table jogadorList={jogadorList} setJogadorList={setJogadorList} />
 
     </>
   )
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,38 +1,97 @@
-import { useState } from 'react';
-import styles from './Form.module.css'
+import { useState } from "react";
+import styles from "./Form.module.css";
 
+export default function Form({ jogadorList, setJogadorList }) {
+  const [formData, setFormData] = useState({
+    nome: "",
+    sobrenome: "",
+    score: "",
+    time: "",
+    estado: "",
+  });
 
-export default function Form() {
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-    const [formData, setFormData] = useState ({
+  // const handleForm = () => {
+  //     const jogadoresArray = [jogadorList, formData];
+  //     localStorage.setItem("jogadores", JSON.stringify(jogadoresArray));
+  // }
+  //o chat gpt disse que o código abaixo é redundante
 
-        nome: '',
-        sobrenome: '',
-        score: '',
-        time: '',
-        estado: '',
-    });
+  const handleForm = () => {
+    const jogadoresArray = [jogadorList, formData];
+    setJogadorList(jogadorList);
 
-    const handleChance = (e) => {
-        setFormData({...formData, [e.target.name]: e.target.value});
+    localStorage.setItem("jogadores", JSON.stringify(jogadoresArray));
+  };
 
+  // <div className="campo">
+  //                         <label> Nome </label>
+  //                         <input name="nome" type="text" placeholder="Digite o Nome" onChange={handleChange}/>
+  //                     </div>
 
-    }
+  return (
+    <section className={styles.formContent}>
+      <h1>Placar do jogo</h1>
+      <form className={styles.form} onSubmit={handleForm}>
+        <div className={styles.input}>
+          <div className={styles.campo}>
+            <label>Nome</label>
+            <input
+              name="nome"
+              type="text"
+              placeholder="Digite o nome"
+              onChange={handleChange}
+            />
+          </div>
 
-    return (
-        <section className={styles.formContent}>
-            <h1>Placar do jogo</h1> 
-            <div className={styles.form}></div>
+          <div className={styles.campo}>
+            <label>Sobrenome</label>
+            <input
+              name="sobrenome"
+              type="text"
+              placeholder="Digite o sobrenome"
+              onChange={handleChange}
+            />
+          </div>
 
+          <div className={styles.campo}>
+            <label>Score</label>
+            <input
+              name="score"
+              type="number"
+              placeholder="Digite a pontuação"
+              onChange={handleChange}
+            />
+          </div>
 
+          <div className={styles.campo}>
+            <label>Time</label>
+            <input
+              name="time"
+              type="text"
+              placeholder="Digite o time"
+              onChange={handleChange}
+            />
+          </div>
 
-        </section>
+          <div className={styles.campo}>
+            <label>Estado</label>
+            <input
+              name="estado"
+              type="text"
+              placeholder="Digite o estado"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
 
-
-
-
-    )
-
-
-
+        <div className={styles.button}>
+          <button>Criar jogador</button>
+        </div>
+      </form>
+    </section>
+  );
 }
