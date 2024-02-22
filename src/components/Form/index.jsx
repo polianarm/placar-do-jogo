58bb22029d802ca1700f952cb12733212d978149
Poliana <polianariveiro@gmail.com>
2024-02-22 18:39:41 -0300

    subindo classes de erro

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,6 +4,8 @@ import styles from "./Form.module.css";
 
 
 export default function Form({ jogadorList, setJogadorList }) {
+  
+  const [formErros, setFormErrors] = useState({});
   const [formData, setFormData] = useState({
     nome: "",
     sobrenome: "",
@@ -14,32 +16,70 @@ export default function Form({ jogadorList, setJogadorList }) {
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    if (formErros[e.target.name]) {
+      setFormErrors({...formErros, [e.target.name]: ''});
+    }
   };
 
-  // const handleForm = () => {
-  //     const jogadoresArray = [jogadorList, formData];
-  //     localStorage.setItem("jogadores", JSON.stringify(jogadoresArray));
-  // }
-  //o chat gpt disse que o código abaixo é redundante
+  const handleForm = (e) => {
 
-  const handleForm = () => {
+    e.preventDefault()
+
+    if(!validForm()) {
+
+      return;
+    }
+
     const jogadoresArray = [...jogadorList, formData];
     setJogadorList(jogadorList);
 
     localStorage.setItem("jogadores", JSON.stringify(jogadoresArray));
+
+    setFormData({
+      nome: "",
+      sobrenome: "",
+      score: "",
+      time: "",
+      estado: "",
+    });
   };
 
-  // <div className="campo">
-  //                         <label> Nome </label>
-  //                         <input name="nome" type="text" placeholder="Digite o Nome" onChange={handleChange}/>
-  //                     </div>
+//nome, sobrenome, score, time, estado
+  const validForm = () => {
+    let errors = {};
+
+
+    if(formData.nome.trim() === '') {
+      errors.nome='O nome é obrigatório!';
+    }
+
+    if(formData.sobrenome.trim() === '') {
+      errors.sobrenome='O sobrenome é obrigatório!';
+    }
+
+    if(formData.score.length <= 0) {
+      errors.score='O score não pode ser 0!';
+    }
+
+    if(formData.time.trim() === '') {
+      errors.time='O jogador precisa ter um time!';
+    }
+
+    if(formData.estado.trim() === '') {
+      errors.estado='O estado é obrigatório';
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  }
 
   return (
     <section className={styles.formContent}>
       <h1>Cadastrar Jogador</h1>
       <form className={styles.form} onSubmit={handleForm}>
         <div className={styles.input}>
-          <div className={styles.campo}>
+          <div className={formErros.nome ? `${styles.campo} ${styles.error}` : styles.campo}>
             <label>Nome</label>
             <input
               name="nome"
@@ -47,9 +87,10 @@ export default function Form({ jogadorList, setJogadorList }) {
               placeholder="Digite o nome"
               onChange={handleChange}
             />
+            {formErros.nome && <span>{formErros.nome}</span>}
           </div>
 
-          <div className={styles.campo}>
+          <div className={formErros.sobrenome ? `${styles.campo} ${styles.error}` : styles.campo}>
             <label>Sobrenome</label>
             <input
               name="sobrenome"
@@ -57,9 +98,10 @@ export default function Form({ jogadorList, setJogadorList }) {
               placeholder="Digite o sobrenome"
               onChange={handleChange}
             />
+             {formErros.sobrenome && <span>{formErros.sobrenome}</span>}
           </div>
 
-          <div className={styles.campo}>
+          <div className={formErros.score ? `${styles.campo} ${styles.error}` : styles.campo}>
             <label>Score</label>
             <input
               name="score"
@@ -67,9 +109,10 @@ export default function Form({ jogadorList, setJogadorList }) {
               placeholder="Digite a pontuação"
               onChange={handleChange}
             />
+             {formErros.score && <span>{formErros.score}</span>}
           </div>
 
-          <div className={styles.campo}>
+          <div className={formErros.time ? `${styles.campo} ${styles.error}` : styles.campo}>
             <label>Time</label>
             <input
               name="time"
@@ -77,9 +120,10 @@ export default function Form({ jogadorList, setJogadorList }) {
               placeholder="Digite o time"
               onChange={handleChange}
             />
+             {formErros.time && <span>{formErros.time}</span>}
           </div>
 
-          <div className={styles.campo}>
+          <div className={formErros.estado ? `${styles.campo} ${styles.error}` : '${styles.campo}'}>
             <label>Estado</label>
             <input
               name="estado"
@@ -87,6 +131,7 @@ export default function Form({ jogadorList, setJogadorList }) {
               placeholder="Digite o estado"
               onChange={handleChange}
             />
+             {formErros.estado && <span>{formErros.estado}</span>}
           </div>
         </div>
 
